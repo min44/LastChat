040cfbd993d380c051964f66ac41e56e413b400f
mikhail.smirnov@bimgen.app <mikhail.smirnov@bimgen.app>
2022-06-04 03:52:11 +0700

    Awesome chat implementation.

--- Client/Program.fs
+++ Client/Program.fs
@@ -3,30 +3,48 @@
 open Browser
 open Elmish
 open Elmish.React
+open Fable.Core.JS
 open Fable.React
 open Props
 
-type Model = { State: string }
+type Model = { State: string
+               TextBox: string
+               Chat: string list }
 
 type Msg =
     | SentRequest
+    | SetTextBox of string
     | SetResponse of string
+    | SetChat of string
 
-let init() = { State = "Initial state" }, []
-let webSocket = WebSocket.Create("ws://127.0.0.1:8080/websocket")
-let sendRequest  = fun _ -> async { webSocket.send("Hello!") } |> Async.StartImmediate
+let webSocket = WebSocket.Create("ws://192.168.68.100:8080/websocket")
+let registerOnMessageHandler =
+    fun dispatch ->
+        async {
+            webSocket.addEventListener_message (fun xy -> dispatch <| SetChat (xy.data.ToString()))
+        } |> Async.StartImmediate
+
+let init() = { State = "Initial state"
+               TextBox = ""
+               Chat = [] }, [ registerOnMessageHandler ]
+let sendRequest model = fun _ -> async { webSocket.send($"{model.TextBox}") } |> Async.StartImmediate
     
 let update msg model =
     match msg with
-    | SentRequest   -> model, [ sendRequest ]
-    | SetResponse r -> { model with State = r }, Cmd.none
+    | SentRequest    -> model, [ sendRequest model ]
+    | SetTextBox v   -> { model with TextBox = v }, Cmd.none
+    | SetResponse r  -> { model with State = r }, Cmd.none
+    | SetChat x      -> { model with Chat = x::model.Chat }, Cmd.none
+
 
 let view (model: Model) dispatch =
     div [] [
         div [] [ str model.State ]
-        button [ OnClick (fun _ -> dispatch SentRequest) ] [ str "SendRequest" ] ]
+        button [ OnClick (fun _ -> dispatch SentRequest) ] [ str "SendRequest" ]
+        div [  ] [ input [ Value model.TextBox; OnChange (fun e -> dispatch (SetTextBox e.Value) ) ] ]
+        str (model.Chat.ToString()) ]
 
 Program.mkProgram init update view
+//|> Program.withSubscription onMessage 
 |> Program.withReactSynchronous "elmish-app"
-|> Program.withConsoleTrace
 |> Program.run
--- Server/Program.fs
+++ Server/Program.fs
@@ -2,18 +2,52 @@
 
 open System.IO
 open Suave
+open Suave.Http
 open Suave.Sockets.Control
 open Suave.Utils
 open Filters
 open Operators
 open WebSocket
+open Suave.Sockets
+
+type State = {Subscribers:WebSocket list}
+
+type Msg =
+    | SendAll of ByteSegment
+    | Subscribe of WebSocket
+
+let processor = MailboxProcessor<Msg>.Start(fun inbox ->
+    let rec innerLoop state  = async {
+        let! message = inbox.Receive()
+        match message with
+        | SendAll msg -> 
+//            state.Subscribers|>List.iter (fun x ->  x.send Text msg true)
+            for x in state.Subscribers do
+                let! result = x.send Text msg true
+                ()
+            do! innerLoop state
+        | Subscribe ws ->
+            let state = { state with Subscribers = ws::state.Subscribers } 
+            do! innerLoop state
+        ()
+         }
+    innerLoop {Subscribers=[]})
 
 let ws (webSocket : WebSocket) _ =
+    processor.Post(Subscribe webSocket)
     socket {
-        let! _, input, _ = webSocket.read()
-        let text = ASCII.toString input
-        printfn $"{text}" }
-  
+        let mutable loop = true
+        
+        while loop do
+            let! _, input, _ = webSocket.read()
+            let text = ASCII.toString input
+            let byteResponse =
+                text
+                |> System.Text.Encoding.ASCII.GetBytes
+                |> ByteSegment
+//            do! webSocket.send Text byteResponse true
+            processor.Post(SendAll byteResponse )
+         }
 let app: WebPart =
      choose [
           GET >=> path "/" >=> Files.file "./public/index.html"    
@@ -22,6 +56,10 @@ let app: WebPart =
           path "/websocket" >=> handShake ws
           RequestErrors.NOT_FOUND "Page not found." ]
 
-let config = { defaultConfig with homeFolder = Some(Path.GetFullPath "./public") }
+let config = {
+    defaultConfig with
+        bindings = [HttpBinding.createSimple Protocol.HTTP "192.168.68.100" 8080]
+        homeFolder = Some(Path.GetFullPath "./public")
+}
 
 startWebServer config app
